31b68a63002940f530823a843a03c47f7d0a5df4
Jeff Jacobs <jjacobs@axialhealthcare.com>
2021-01-07 09:18:55 -0600

    Remove unnecessary scss file

--- src/views/State/State.tsx
+++ src/views/State/State.tsx
@@ -1,7 +1,6 @@
 import React, { Fragment, useState } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
-import './State.scss';
 import { Todo } from '../../interfaces/Todos-Interface';
 import StateLocal from './State-Local';
 import StateGlobal from './State-Global';
@@ -9,16 +8,15 @@ import StateParent from './State-Parent';
 import StateNav from './State-Nav';
 
 function State() {
-
   const initialTodosForChild: Todo[] = [
     {
       value: 'Clean the kitchen',
-      done: false
+      done: false,
     },
     {
       value: 'Wash the car',
-      done: true
-    }
+      done: true,
+    },
   ];
 
   const [todosForChild, setTodosForChild] = useState<Todo[]>(initialTodosForChild);
@@ -30,14 +28,17 @@ function State() {
         <div className="Content Content-Padded">
           <Switch>
             <Route path="/state/local" component={StateLocal} />
-            <Route path="/state/parent" component={() => <StateParent todos={todosForChild} setTodos={setTodosForChild} /> }/>
+            <Route
+              path="/state/parent"
+              component={() => <StateParent todos={todosForChild} setTodos={setTodosForChild} />}
+            />
             <Route path="/state/global" component={StateGlobal} />
             <Redirect to="/state/local" />
           </Switch>
         </div>
       </Fragment>
     </div>
-  )
+  );
 }
 
 export default State;
